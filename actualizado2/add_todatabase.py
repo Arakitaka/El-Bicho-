427839b0465ba0a486307bd4161adb15dbe2a8a9
az <93015718+FoolishBishop@users.noreply.github.com>
2023-12-27 16:20:30 -0300

    Update add_todatabase.py

--- actualizado2/add_todatabase.py
+++ actualizado2/add_todatabase.py
@@ -1,4 +1,14 @@
 import random
+from csv import writer
+
+
+def load_val_csv(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
 
 
 def coordenadas_random():
@@ -11,8 +21,9 @@ hexcode = input("hexcode >>> ")
 objecto = input("object >>> ")
 owner = input("owner >>> ")
 location = coordenadas_random()
-el_lista = (hexcode, objecto, owner, str(location))
+el_lista = [hexcode, objecto, owner, str(location)]
 print(el_lista)
 
-append_list_as_row(el_lista)
+name_file = "mecadb.csv"
+load_val_csv(name_file, el_lista)
 print("funco")
