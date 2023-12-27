a4fabe59f3daa430e3d41a40a5d47690fec647ef
az <93015718+FoolishBishop@users.noreply.github.com>
2023-12-27 16:21:58 -0300

    Update rfidreader2.py

--- actualizado2/rfidreader2.py
+++ actualizado2/rfidreader2.py
@@ -1,19 +1,42 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import pandas as pd
+from time import sleep
+
+# Nota: el pandas solo lee el hex como int (amen)
 
 df = pd.read_csv('mecadb.csv')
 
 
-class RFID:
-    def read(self):
-        reader = SimpleMFRC522()
+def read_rfid():
+    reader = SimpleMFRC522()
+    while True:
         try:
             card_id, _ = reader.read()
             print(card_id)
-            bolb = df.loc[df['hex'] == str(card_id)]
-            for index, row in bolb.iterrows():
-                print(index, row['Name'], row['HP'])
-
+            return card_id
         finally:
             GPIO.cleanup()
+
+
+def find_file(hexcode):
+    # encuentra la fila (o filas) con el valor "x" en df['columna']
+    fila_delhex = df.loc[df['hex'] == hexcode]
+    # coloca los valores de la fila en variables
+    h3x, obj, own, loc = [fila_delhex[row].tolist() for row in fila_delhex]
+    # como las variables estan en listas y no me gusta las vuelvo not-listas
+    h3x, obj, own, loc = h3x[0], obj[0], own[0], loc[0]
+    # stack overflow my beloved
+    return h3x, obj, own, loc
+
+
+def main():
+    while True:
+        hexcode = read_rfid()
+        sleep(1)
+        tup_hool = find_file(hexcode)
+        print(tup_hool)
+
+
+# main
+main()
